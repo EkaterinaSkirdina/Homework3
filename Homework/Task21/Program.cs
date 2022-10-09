527252029fb67ef94aa928950f29c6a06e459239
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2022-10-09 19:35:05 +0300

    Task 21

--- Homework/Task21/Program.cs
+++ Homework/Task21/Program.cs
@@ -22,6 +22,6 @@ int y2 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите z второй точки: ");
 int z2 = int.Parse(Console.ReadLine());
 
-double Distance = Math.Sqrt(Math.Pow(x2 - x1, 2)+Math.Pow(y2 - y1, 2)+Math.Pow(z2 - z1, 2));
+double Distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
 
-Console.WriteLine($"Расстояние между точками в 3D пространстве составит {Distance}");
+Console.WriteLine($"Расстояние между точками в 3D пространстве составит {Math.Round((Distance), 2)}");
